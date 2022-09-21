edd079c999b248b7756d370ec30b4f7b636b33e8
Fernanda <fernandakagami@gmail.com>
2022-09-21 17:24:21 -0300

    update home

--- 04-project/assets/scripts/card-hover-animation.js
+++ 04-project/assets/scripts/card-hover-animation.js
@@ -1,5 +1,11 @@
 function handleMouseEnter() {
     this.classList.add('s-card--hovered');
+    document.body.id = `${this.id}-hovered`;
+}
+
+function handleMouseLeave() {
+    this.classList.remove('s-card--hovered');
+    document.body.id = '';
 }
 
 function addEventListenersToCards() {
@@ -8,6 +14,7 @@ function addEventListenersToCards() {
     for (let index = 0; index < cardElements.length; index++) {
         const card = cardElements[index];
         card.addEventListener('mouseenter', handleMouseEnter);
+        card.addEventListener('mouseleave', handleMouseLeave);
     }
 }
 
