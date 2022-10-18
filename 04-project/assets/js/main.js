5bd41188ec604990a73d5c5452a049e868801f14
Fernanda <fernandakagami@gmail.com>
2022-10-18 10:24:41 -0300

    update project

--- 04-project/assets/js/main.js
+++ 04-project/assets/js/main.js
@@ -1,11 +1,21 @@
 const form = document.getElementById('newItem');
+const list = document.getElementById("list");    
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log(event);
+
+    list.innerHTML += elementCreate(event.target.elements['name'].value);
+    
     console.log(event.target.elements['name'].value);
+
+    localStorage.setItem("name", event.target.elements['name'].value);
+    event.target.elements['name'].value = '';
+
 });
 
-function elementCreate() {
-    
+function elementCreate(name) {
+    return `<li class="task">
+            <i class="fa-regular fa-square"></i>
+            <p>${name}</p>
+        </li>`
 }
