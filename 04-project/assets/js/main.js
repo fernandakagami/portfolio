848ceb73d828426113cf0e56b0440eadf0936e0f
Fernanda <fernandakagami@gmail.com>
2022-10-18 11:09:44 -0300

    Update main.js

--- 04-project/assets/js/main.js
+++ 04-project/assets/js/main.js
@@ -1,16 +1,24 @@
 const form = document.getElementById('newItem');
-const list = document.getElementById("list");    
+const list = document.getElementById("list"); 
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
+
+console.log(itens);
+
+itens.forEach(element => {
+    list.innerHTML += elementCreate(element.name);
+});
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
     list.innerHTML += elementCreate(event.target.elements['name'].value);
-    
-    console.log(event.target.elements['name'].value);
 
-    localStorage.setItem("name", event.target.elements['name'].value);
+    storage(event.target.elements['name'].value);
+
+    //itens.push(event.target.elements['name'].value);
+    //localStorage.setItem("itens", itens);
+
     event.target.elements['name'].value = '';
-
 });
 
 function elementCreate(name) {
@@ -18,4 +26,13 @@ function elementCreate(name) {
             <i class="fa-regular fa-square"></i>
             <p>${name}</p>
         </li>`
+}
+
+function storage(name) {
+    const currentItem = {
+        "name": name
+    }
+
+    itens.push(currentItem);
+    localStorage.setItem("itens", JSON.stringify(itens));
 }
