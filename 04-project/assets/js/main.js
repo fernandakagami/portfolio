f7f07ce3aa9ce11c06070ec475736d9e95c2e302
Fernanda <fernandakagami@gmail.com>
2022-10-18 15:34:45 -0300

    Update main.js

--- 04-project/assets/js/main.js
+++ 04-project/assets/js/main.js
@@ -5,34 +5,65 @@ const itens = JSON.parse(localStorage.getItem("itens")) || [];
 console.log(itens);
 
 itens.forEach(element => {
-    list.innerHTML += elementCreate(element.name);
+    console.log(element.checked)
+    if (element.checked === true) {
+        list.innerHTML += elementCheckedCreate(element.name);   
+    } else {
+        list.innerHTML += elementCreate(element.name);   
+    }    
 });
 
 form.addEventListener('submit', (event) => {
-    event.preventDefault();
+    event.preventDefault();    
 
-    list.innerHTML += elementCreate(event.target.elements['name'].value);
+    if (event.target.elements['name'].value != '') {
+        list.innerHTML += elementCreate(event.target.elements['name'].value);
 
-    storage(event.target.elements['name'].value);
+        storage(event.target.elements['name'].value);
 
-    //itens.push(event.target.elements['name'].value);
-    //localStorage.setItem("itens", itens);
-
-    event.target.elements['name'].value = '';
+        event.target.elements['name'].value = '';
+    } else {
+        event.target.elements['name'].value = '';
+    }
 });
 
 function elementCreate(name) {
     return `<li class="task">
-            <i class="fa-regular fa-square"></i>
-            <p>${name}</p>
+                <i class="fa-regular fa-square"></i>
+                <p class="task-description">${name}</p>
         </li>`
 }
 
-function storage(name) {
+function elementCheckedCreate(name) {
+    return `<li class="task">
+                <i class="fa-regular fa-square-check"></i>
+                <p class="task-description">${name}</p>
+        </li>`
+}
+
+function storage(name, checked = false) {
     const currentItem = {
-        "name": name
+        "name": name,
+        "checked": checked
     }
 
     itens.push(currentItem);
     localStorage.setItem("itens", JSON.stringify(itens));
 }
+
+// check box
+
+const checks = document.querySelectorAll(".task");
+
+for (let index = 0; index < checks.length; index++) {
+    checks[index].addEventListener('click', () => {
+        let check = checks[index].querySelector(".fa-square");
+        check.classList.remove('fa-square')
+        check.classList.add('fa-square-check');
+
+        itens[index].checked = true;             
+    });    
+    localStorage.setItem("itens", JSON.stringify(itens));
+}
+
+
