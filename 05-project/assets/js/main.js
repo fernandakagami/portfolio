a5bd0020e56d6250cb3b8a13a101f380e1f18bb5
Fernanda <fernandakagami@gmail.com>
2022-10-19 09:37:26 -0300

    update display screen

--- 05-project/assets/js/main.js
+++ 05-project/assets/js/main.js
@@ -1,49 +1,20 @@
-let keys = document.querySelectorAll(".key");
+const keys = document.querySelectorAll(".key");
 
 for (let index = 0; index < keys.length; index++) {
-    keys[index].addEventListener("click", function() {
+    keys[index].addEventListener("click", () => {
 
         buttonInnerHTML = keys[index].innerHTML;
 
         console.log(buttonInnerHTML);
 
-        makeCalc(buttonInnerHTML);
+        // show the input in screen
+        const result = document.querySelector(".result");
+        result.innerHTML = buttonInnerHTML;
 
-
-    });
-    
+    });    
 }
 
-function makeCalc(key) {
-    switch (key) {
-        case 1:
-                           
-            break;
-        case 2:
-                
-            break;
-        case "three":
-                
-            break;
-        case "four":
-                
-            break;
-        case "five":
+function calc (num1, operator, num2) {
 
-            break;
-        case "six":
-                
-            break;
-        case "seven":
-            
-        break;
-        case "eight":
-            
-        break;
-        case "nine":
-            
-        break;
-        
-        default: console.log("erro");
-    }
 }
+
