903c2b2a83b6fdb3f0417f62660de06c90f12eab
Fernanda <fernandakagami@gmail.com>
2022-10-30 17:06:10 -0300

    Update search-button.js

--- project06-pokedex/assets/js/search-button.js
+++ project06-pokedex/assets/js/search-button.js
@@ -1,6 +1,13 @@
-let coll = document.querySelector(".collapsible");
-coll.addEventListener("click", function() {
-    var content = this.previousElementSibling;
+const coll = document.querySelector(".collapsible");
+const content = document.querySelector(".content-search");
+const closeButton = document.querySelector(".close");
+
+coll.addEventListener("click", function() {    
     content.style.display = "flex";    
-    coll.remove();
+    coll.style.display = "none";
+})
+
+closeButton.addEventListener("click", function() {
+    content.style.display = "none";
+    coll.style.display = "inline-block";
 })
