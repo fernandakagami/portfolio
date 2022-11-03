866ee44434b7f65e25e96e107e777fe88c6a66f6
Fernanda <fernandakagami@gmail.com>
2022-11-02 21:26:01 -0300

    Update main.js

--- project06-pokedex/assets/js/main.js
+++ project06-pokedex/assets/js/main.js
@@ -1,6 +1,7 @@
 const pokemonList = document.querySelector(".pokemons");
 const loadMoreButton = document.getElementById('loadMore');
 const pokemon = document.querySelector(".pokemon");
+const pokeball = document.querySelector(".pokeball");
 const maxRecords = 151;
 const limit = 10;
 let offset = 0;
@@ -8,7 +9,7 @@ let chartPokemons = [];
 let color = '';
 let pokemonInput = document.querySelector('.pokemon-input').value;
 
-function loadPokemonItens(offset, limit) {
+function loadPokemonItens(offset, limit) {    
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
     
         pokemons.forEach((pokemon) => {
@@ -19,20 +20,26 @@ function loadPokemonItens(offset, limit) {
 
 loadPokemonItens(offset, limit);
 
-loadMoreButton.addEventListener('click', () => {
+function removeRotation() {
+    pokeball.classList.remove("rotation");
+}
+
+loadMoreButton.addEventListener('click', () => {    
     offset += limit;
     const qtdRecordsWithNexPage = offset + limit;    
-       
+        
     if (qtdRecordsWithNexPage >= maxRecords) {
         const newLimit = maxRecords - offset;
         loadPokemonItens(offset, newLimit);
 
         loadMoreButton.parentElement.removeChild(loadMoreButton);
-    } else {
-        loadPokemonItens(offset, limit);       
-        
+    } else {                 
+        pokeball.classList.add("rotation");
+        setTimeout(loadPokemonItens, 1000, offset, limit);
+        setTimeout(removeRotation, 1000);       
     }
-})
+    
+});
 
 
     
