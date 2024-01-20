de01bd36d3186a290217430efd1feb718007879c
fernanda <fernandakagami@gmail.com>
2024-01-20 20:11:02 -0300

    update bar and create routes

--- project01/front/src/app/components/bar.js
+++ project01/front/src/app/components/bar.js
@@ -7,50 +7,84 @@ import { Icon } from '@iconify/react';
 export default function Bar() {
   const [date, setDate] = useState(new Date());
   const [open, setOpen] = useState(false);
-  let [count, setCount] = useState(0);
+  let [room, setRoom] = useState(1);
+  let [guest, setGuest] = useState(1);
 
-  function incrementCount() {
-    count = count + 1;
-    setCount(count);
-  }
-  function decrementCount() {
-    count = count - 1;
-    setCount(count);
+  const [reservation, setReservation] = useState({ room: 1, guest: 1 })
+
+  const handleSetReservation = () => {
+    setReservation({ room, guest })
+    setOpen(false)
   }
 
   return (
-    <div className="absolute bg-white bottom-44 rounded-full flex items-center justify-center" style={{ width: "1000px", height: "100px", bottom: "170px" }}>
+    <div className="bg-white bottom-44 rounded-full flex items-center justify-center" style={{ width: "800px", height: "100px", transform: "translateY(-265px)" }}>
       <form className="flex flex-row" >
         <div className="flex flex-col items-end border-e pe-8">
           <div className="flex gap-2 items-center">
             <Icon icon="clarity:calendar-line" className="text-[#24AB70] text-2xl" />
             <h3 className="text-[#222222] text-2xl">Check-in</h3>
           </div>
-          <DatePicker selected={date} onChange={date => setDate(date)} style={{ color: "black" }} dateFormat="dd MMM yyyy" className="text-end" />
+          <DatePicker selected={date} onChange={date => setDate(date)} style={{ color: "black" }} dateFormat="dd MMM yyyy" className="text-end cursor-pointer" />
         </div>
         <div className="flex flex-col items-end border-e pe-8">
           <div className="flex gap-2 items-center">
             <Icon icon="clarity:calendar-line" className="text-[#24AB70] text-2xl" />
             <h3 className="text-[#222222] text-2xl">Check-out</h3>
           </div>
-          <DatePicker selected={date} onChange={date => setDate(date)} style={{ color: "black" }} dateFormat="dd MMM yyyy" className="text-end" />
+          <DatePicker selected={date} onChange={date => setDate(date)} style={{ color: "black" }} dateFormat="dd MMM yyyy" className="text-end cursor-pointer" />
         </div>
-        <div className="relative flex flex-col items-end ps-8" onClick={() => setOpen(!open)}>
+        <div className="relative flex flex-col items-end ps-8 pe-36 cursor-pointer" onClick={() => setOpen(!open)}>
           <div className="flex gap-2 items-center">
             <Icon icon="ri:user-fill" className="text-[#24AB70] text-2xl" />
             <h3 className="text-[#222222] text-2xl">Quartos para</h3>
           </div>
-          <div>1 quarto, 2 pessoas</div>
+          <div>{reservation?.room} quarto, {reservation?.guest} pessoas</div>
+        </div>
+        <div className="cursor-pointer relative">
+          <button className="absolute bg-[#24AB70] text-[#fff] rounded-full top-[-22px] right-[-60px]" style={{ width: "165px", height: "100px" }}>
+            Buscar
+          </button>
         </div>
       </form >
 
       {
-        open && <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" tabIndex="-1">
-          <div className="py-1">
-            <div>
-              <button onClick={incrementCount}>+</button><div>{count}</div><button onClick={decrementCount}>-</button>
+        open && <div className="absolute right-0 z-10 p-3 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none w-48" tabIndex="-1">
+          <div className="py-1 relative">
+            <div className="absolute top-[-5.25px] right-[-6.25px] cursor-pointer" onClick={() => setOpen(false)}>
+              <Icon icon="material-symbols:close" />
             </div>
-            <a href="#" className="text-gray-700 block px-4 py-2 text-sm" tabIndex="-1">Support</a>
+            <p className="mb-2 flex justify-center">Consulta</p>
+            <hr></hr>
+            <div className="mt-3">
+              <div className="flex flex-row justify-between mb-2">
+                <p>Quarto</p>
+                <div className="flex flex-row">
+                  <button onClick={() => setRoom((room) => room + 1)}>+</button>
+                  <div className="w-10">
+                    <p className="text-center">
+                      {room}
+                    </p>
+                  </div>
+                  <button onClick={() => setRoom((room) => Math.max(room - 1, 0))}>-</button>
+                </div>
+              </div>
+              <div className="flex flex-row justify-between">
+                <p>Pessoas</p>
+                <div className="flex flex-row">
+                  <button onClick={() => setGuest((guest) => guest + 1)}>+</button>
+                  <div className="w-10">
+                    <p className="text-center">
+                      {guest}
+                    </p>
+                  </div>
+                  <button onClick={() => setGuest((guest) => Math.max(guest - 1, 0))}>-</button>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-center items-center mt-3 cursor-pointer bg-[#24AB70] rounded-lg py-1" onClick={() => handleSetReservation()}>
+              <button className="text-[#fff]">Incluir</button>
+            </div>
           </div>
         </div>
       }
--- project01/front/src/app/layout.js
+++ project01/front/src/app/layout.js
@@ -1,12 +1,29 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Open_Sans } from 'next/font/google';
+import './globals.css';
+import Link from 'next/link';
 
-const inter = Inter({ subsets: ['latin'] })
+const open = Open_Sans({ weight: ['400', '700'], subsets: ['latin'] });
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt">
-      <body className={inter.className}>{children}</body>
+      <body className={open.className}>
+        <ul className="flex justify-center pt-36">
+          <li className="mr-5 cursor-pointer" style={{ color: "#222222" }}>
+            <Link href="/">Home</Link>
+          </li>
+          <li className="mr-5 cursor-pointer" style={{ color: "#222222" }}>
+            <Link href="/">Reservas</Link>
+          </li>
+          <li className="cursor-pointer" style={{ color: "#222222" }}>
+            <Link href="/">Quartos</Link>
+          </li>
+        </ul>
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
